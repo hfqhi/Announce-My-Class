72da771cb85258bfe5944d60f882964c053f4aeb
Fence <johnrey.aparecio@gmail.com>
2026-04-06 16:55:46 +0800

    Phase 1: Setting up DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // --- OUR NEW SAAS COLUMNS ---
+            $table->string('class_code')->unique()->nullable();
+            $table->string('display_name')->nullable();
+            $table->string('role')->default('admin');
+            // ----------------------------
+
             $table->rememberToken();
             $table->timestamps();
         });
